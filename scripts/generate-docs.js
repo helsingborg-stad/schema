9c4e16dadd3b1958e6ba58f786c75ddd0675b904
Thor Brink <thor.brink@helsingborg.se>
2025-07-04 11:06:57 +0200

    feat: add property acceptsReservations to Place (#2)

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const schemaPath = path.join(__dirname, '../schema/schema.jsonld');
+const schemaPath = path.join(__dirname, '../schema.jsonld');
 const templatePath = path.join(__dirname, '../doc-template/doc-template.html');
 const outputPath = path.join(__dirname, '../index.html');
 
@@ -10,17 +10,34 @@ const graph = schema['@graph'] || [];
 const template = fs.readFileSync(templatePath, 'utf8');
 
 function escapeHtml(str) {
-  return (str || '').replace(/[&<>]/g, c => ({'&':'&amp;','<':'&lt;','>':'&gt;'}[c]));
+  return (str || '').replace(/[&<>]/g, c => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;' }[c]));
 }
 
 function renderProperty(prop) {
+  // Handle domainIncludes and rangeIncludes as array or object
+  function renderDomain(domain) {
+    if (Array.isArray(domain)) {
+      return domain.map(d => escapeHtml(d['@id'] || '')).join(', ');
+    } else if (domain && typeof domain === 'object') {
+      return escapeHtml(domain['@id'] || '');
+    }
+    return '';
+  }
+  function renderRange(range) {
+    if (Array.isArray(range)) {
+      return range.map(r => escapeHtml(r['@id'] || '')).join(', ');
+    } else if (range && typeof range === 'object') {
+      return escapeHtml(range['@id'] || '');
+    }
+    return '';
+  }
   return `<div class="property">
     <h4 id="${escapeHtml(prop['@id'])}">${escapeHtml(prop['rdfs:label'] || prop['@id'])}</h4>
     <p>${escapeHtml(prop['rdfs:comment'] || '')}</p>
     <ul>
       <li><b>Type:</b> ${escapeHtml(prop['@type'])}</li>
-      <li><b>Domain:</b> ${escapeHtml((prop['schema:domainIncludes'] && prop['schema:domainIncludes']['@id']) || '')}</li>
-      <li><b>Range:</b> ${escapeHtml((prop['schema:rangeIncludes'] && prop['schema:rangeIncludes']['@id']) || '')}</li>
+      <li><b>Domain:</b> ${renderDomain(prop['schema:domainIncludes'])}</li>
+      <li><b>Range:</b> ${renderRange(prop['schema:rangeIncludes'])}</li>
     </ul>
   </div>`;
 }
@@ -39,12 +56,23 @@ function renderClass(cls) {
 const classes = graph.filter(e => e['@type'] === 'rdfs:Class');
 const properties = graph.filter(e => e['@type'] === 'rdf:Property');
 
-// Group properties by domain
+
+// Group properties by domain (handle array or object)
 const propertiesByDomain = {};
 properties.forEach(prop => {
-  let domain = (prop['schema:domainIncludes'] && prop['schema:domainIncludes']['@id']) || 'Other';
-  if (!propertiesByDomain[domain]) propertiesByDomain[domain] = [];
-  propertiesByDomain[domain].push(prop);
+  let domainField = prop['schema:domainIncludes'];
+  let domains = [];
+  if (Array.isArray(domainField)) {
+    domains = domainField.map(d => d['@id'] || 'Other');
+  } else if (domainField && typeof domainField === 'object') {
+    domains = [domainField['@id'] || 'Other'];
+  } else {
+    domains = ['Other'];
+  }
+  domains.forEach(domain => {
+    if (!propertiesByDomain[domain]) propertiesByDomain[domain] = [];
+    propertiesByDomain[domain].push(prop);
+  });
 });
 
 const classesHtml = classes.map(renderClass).join('\n');
